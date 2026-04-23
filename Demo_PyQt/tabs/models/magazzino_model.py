a49e90069134128a5f2bf7b188bbee6cafb9f6e6
salvogalati <sgalati95@yahoo.it>
2026-04-23 17:40:13 +0200

    Enhance MagazzinoModel data handling and add YesNoDelegate for improved UI interactions

--- Demo_PyQt/tabs/models/magazzino_model.py
+++ Demo_PyQt/tabs/models/magazzino_model.py
@@ -1,5 +1,7 @@
 from PyQt5.QtSql import QSqlTableModel
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QItemDelegate, QStyledItemDelegate, QComboBox
 
 class MagazzinoModel(QSqlTableModel):
     def __init__(self, db, parent=None):
@@ -13,3 +15,55 @@ class MagazzinoModel(QSqlTableModel):
             return flags & ~Qt.ItemIsEditable
 
         return flags
+    
+    # def data(self, index, role=Qt.DisplayRole):
+    #     col = self.fieldIndex("da_prezzare")
+    #     value = super().data(index, Qt.EditRole)
+
+    #     if index.column() == col:
+
+    #         # if role == Qt.DisplayRole:
+    #         #     return value  # già "Si" / "No"
+
+    #         if role == Qt.BackgroundRole:
+    #             if value == "Si":
+    #                 return QColor("#e7f34b")
+
+    #     return super().data(index, role)
+    
+    def data(self, index, role=Qt.DisplayRole):
+        col = self.fieldIndex("da_prezzare")
+        value = super().data(index, Qt.EditRole)
+
+        if index.column() == col:
+
+            if role == Qt.DisplayRole:
+                return value  # "Si" / "No"
+
+            if role == Qt.BackgroundRole:
+                if value == "Si":
+                    return QColor("#e7f34b")
+                else: print(index.row(), value)
+
+        return super().data(index, role)
+    
+    
+
+
+class YesNoDelegate(QItemDelegate):
+
+    def createEditor(self, parent, option, index):
+        combo = QComboBox(parent)
+        combo.addItems(["No", "Si"])
+        return combo
+
+    def setEditorData(self, editor, index):
+        value = index.model().data(index, Qt.EditRole)
+        editor.setCurrentText("Si" if value in (1, True, "1") else "No")
+
+    def setModelData(self, editor, model, index):
+        model.setData(index, editor.currentText(), Qt.EditRole)
+        model.dataChanged.emit(index, index)
+
+    def updateEditorGeometry(self, editor, option, index):
+        editor.setGeometry(option.rect)
