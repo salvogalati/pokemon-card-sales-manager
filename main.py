ad234fe250000b1b9bc3f701eb42a6fbea475cd9
salvogalati <sgalati95@yahoo.it>
2026-05-05 14:59:04 +0200

    Add error handling dialog and exception hook for improved error reporting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import traceback
 from PyQt5 import QtWidgets, uic, QtSql
 from tabs.magazzino import MagazzinoTabController
 from tabs.acquisti import AcquistiTabController
@@ -36,9 +37,44 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.adjustSize()
 
+class ErrorDialog(QtWidgets.QDialog):
+    def __init__(self, error_text, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Errore inatteso")
+        self.resize(700, 500)
+
+        layout = QtWidgets.QVBoxLayout(self)
+
+        self.text = QtWidgets.QTextEdit()
+        self.text.setReadOnly(True)
+        self.text.setPlainText(error_text)
+
+        layout.addWidget(self.text)
+
+        btn = QtWidgets.QPushButton("Chiudi")
+        btn.clicked.connect(self.close)
+        layout.addWidget(btn)
+
+def qt_exception_hook(type, value, tb):
+    sys.__excepthook__(type, value, tb)
+    show_exception_box(type, value, tb)
+
+def show_exception_box(exc_type, exc_value, exc_traceback):
+    msg = "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+
+    try:
+        app = QtWidgets.QApplication.instance()
+        if app:
+            dlg = ErrorDialog(msg)
+            dlg.exec_()
+        else:
+            print(msg)
+    except:
+        print(msg)
 
 
 if __name__ == "__main__":
+    sys.excepthook = qt_exception_hook
     app = QtWidgets.QApplication(sys.argv)
 
     # Stylesheet moderno
