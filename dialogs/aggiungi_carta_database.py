9b79efe9b08a9c149b3e83ecd967e7e4987f4ca5
salvogalati <sgalati95@yahoo.it>
2026-05-06 17:03:33 +0200

    Add functions to add and delete cards from database

---
+++ dialogs/aggiungi_carta_database.py
@@ -0,0 +1,59 @@
+from PyQt5 import QtWidgets, uic, QtSql
+from config import get_resource_path, database_table
+from utils import createMessageBox
+import os
+import traceback
+
+class AggiungiCartaDatabaseDialog(QtWidgets.QDialog):
+    def __init__(self, data, parent=None):
+        super().__init__()
+        self.data = data
+        self.db_cards = QtSql.QSqlDatabase.database("card_db_connection")
+        # Carica il file .ui
+        uic.loadUi(get_resource_path(os.path.join("ui", "dialog_aggiungi_carta.ui")), self)
+
+
+    def accept(self):
+        card_data = {
+            "id": self.lineEditID.text().strip(),
+            "name": self.lineEditNome.text().strip(),
+            "espansione_id": self.lineEditEspansioneID.text().strip(),
+            "espansione_nome": self.lineEditEspansioneNome.text().strip(),
+            "image": self.lineEditImageURL.text().strip(),
+            "pricing_cardmarket": self.doubleSpinBox.value()}
+        if not all(list(card_data.values())):
+            msg = createMessageBox("Errore", "Compila tutti i campi prima di aggiungere la carta.")
+            msg.exec_()
+            return
+        try:
+            insert_query = QtSql.QSqlQuery(self.db_cards)
+            print(card_data)
+            insert_query.prepare(f"""
+                INSERT INTO {database_table} (id, name, espansione_id, espansione_nome, image, pricing_cardmarket_low)
+                VALUES (:id, :name, :espansione_id, :espansione_nome, :image, :pricing_cardmarket_low)
+            """)
+            insert_query.bindValue(":id", card_data["id"])
+            insert_query.bindValue(":name", card_data["name"])
+            insert_query.bindValue(":espansione_id", card_data["espansione_id"])
+            insert_query.bindValue(":espansione_nome", card_data["espansione_nome"])
+            insert_query.bindValue(":image", card_data["image"])
+            insert_query.bindValue(":pricing_cardmarket_low", card_data["pricing_cardmarket"])
+            if not insert_query.exec_():
+                msg = createMessageBox("Errore", f"Errore durante l'inserimento della carta: {insert_query.lastError().text()}")
+                msg.exec_()
+                return
+            self.db_cards.commit()
+            msg = createMessageBox("Successo", "Carta aggiunta con successo al database!")
+            msg.exec_()
+        except Exception as e:
+            self.db_cards.rollback()
+            msg = createMessageBox(
+                "Errore",
+                f"Errore durante l'inserimento della carta:\n{str(e)}",
+                QtWidgets.QMessageBox.Critical,
+            )
+            msg.exec_()
+            traceback.print_exc()
+            return
+
+        super().accept()
--- tabs/database.py
+++ tabs/database.py
@@ -1,11 +1,14 @@
 
-from PyQt5 import QtSql
+import traceback
+
+from PyQt5 import QtSql, QtWidgets
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import QObject, Qt
 import requests
 
 from .models.card_database_model import CardDatabaseModel
-from utils import createMessageBox
+from dialogs.aggiungi_carta_database import AggiungiCartaDatabaseDialog
+from utils import createMessageBox, pulisci_testo
 from config import database_table
 from icons import icons  # noqa: F401
 
@@ -25,12 +28,18 @@ class DatabaseTabController(QObject):
         self.ui.tableViewDatabase.selectionModel().selectionChanged.connect(self.on_row_changed)
 
         self.ui.lineEditSearchDatabase.textChanged.connect(self.filtra_tabella)
+        self.ui.buttonAggiungiCartaDatabase.clicked.connect(self.apri_dialog_aggiungi_carta)
+        self.ui.buttonRimuoviCartaDatabase.clicked.connect(self.rimuovi_carta_selezionata)
 
     def filtra_tabella(self, testo):
+        testo = pulisci_testo(testo)
         if not testo:
             self.model_card_database.setFilter("")
         else:
-            filtro = f"nome LIKE '%{testo}%' OR espansione LIKE '%{testo}%'"
+            filtro = f"""name LIKE '%{testo}%'
+            OR espansione_nome LIKE '%{testo}%'
+            OR id LIKE '%{testo}%'
+            OR espansione_id LIKE '%{testo}%'"""
             self.model_card_database.setFilter(filtro)
 
     def on_row_changed(self, selected, deselected):
@@ -79,3 +88,56 @@ class DatabaseTabController(QObject):
 
         except Exception:
             return None
+        
+    def apri_dialog_aggiungi_carta(self):
+        dialog = AggiungiCartaDatabaseDialog({}, self.ui)
+        if dialog.exec_():
+            self.model_card_database.select()
+
+    def rimuovi_carta_selezionata(self):
+        selected_indexes = self.ui.tableViewDatabase.selectionModel().selectedIndexes()
+        rows = set(index.row() for index in selected_indexes)
+        if not rows:
+            msg = createMessageBox("Errore", "Seleziona una carta da rimuovere.")
+            msg.exec_()
+            return
+        selected_row = list(rows)[0]
+        card_id_index = self.model_card_database.index(selected_row, self.model_card_database.fieldIndex("id"))
+        card_id = self.model_card_database.data(card_id_index)
+
+        confirm_msg = createMessageBox(
+            "Conferma Rimozione",
+            f"Sei sicuro di voler rimuovere la carta con ID '{card_id}' dal database?",
+            QtWidgets.QMessageBox.Warning,
+            [QtWidgets.QMessageBox.StandardButton.Yes, QtWidgets.QMessageBox.StandardButton.No],
+        )
+        risposta = confirm_msg.exec_()
+        if risposta == QtWidgets.QMessageBox.StandardButton.No:
+            return
+        try:
+            delete_query = QtSql.QSqlQuery(self.db)
+            delete_query.prepare(f"DELETE FROM {database_table} WHERE id = ?")
+            delete_query.addBindValue(card_id)
+            if not delete_query.exec_():
+                msg = createMessageBox(
+                    "Errore",
+                    f"Errore durante la rimozione della carta: {delete_query.lastError().text()}",
+                    QtWidgets.QMessageBox.Critical
+                )
+                msg.exec_()
+                return
+            self.model_card_database.select()
+            msg = createMessageBox("Successo", "Carta rimossa con successo dal database!")
+            msg.exec_()
+        except Exception as e:
+            self.db.rollback()
+            msg = createMessageBox(
+                "Errore",
+                f"Errore durante la rimozione della carta:\n{str(e)}",
+                QtWidgets.QMessageBox.Critical,
+            )
+            msg.exec_()
+            traceback.print_exc()
+            return
+
+        
